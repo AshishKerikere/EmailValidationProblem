fb857e30658377ba4c7462a7561e4a622f939b13
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-02 15:29:47 +0530

    Email Validation UC1

--- EmailValidationTest.java
+++ EmailValidationTest.java
@@ -1,1 +1,134 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class EmailValidationTest {
 
+    public static void main(String[] args) {
+
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Email address");
+        String emailAddress = scanner.next();*/
+
+        evaluateTypeAndCheck("abc@yahoo.com");
+
+    }
+
+    public static void evaluateTypeAndCheck(String emailAddress) {
+
+        int atSymbolCount = 0;
+        int dotSymbolCount = 0;
+        int atSymbolLocation = 0;
+        int[] dotSymbolLocations = new int[3];
+
+        for (int i = 0; i<emailAddress.length(); i++){
+            if (emailAddress.charAt(i) == '.'){
+                dotSymbolCount++;
+                dotSymbolLocations[dotSymbolCount-1] = i;
+            }
+            if (emailAddress.charAt(i) == '@'){
+                atSymbolCount++;
+                atSymbolLocation = i;
+            }
+        }
+
+        if (atSymbolCount == 1 && dotSymbolCount == 1){
+            if (atSymbolLocation < dotSymbolLocations[0]){
+                if (mandatoryPrefixCheck(emailAddress, atSymbolLocation, dotSymbolLocations)
+                        && mandatoryDomainCheck(emailAddress, atSymbolLocation, dotSymbolLocations)
+                        && mandatoryString3Check(emailAddress, dotSymbolLocations, 1)){
+                    System.out.println("Case1 syntax is correct and matches Regex Pattern");
+                }
+                else{
+                    System.out.println("Case 1 syntax is correct but doesnot match Regex Patterns");
+                }
+            }
+            else {
+                System.out.println("Invalid Case 1");
+            }
+        }
+
+        else if (atSymbolCount == 1 && dotSymbolCount == 3) {
+            if (atSymbolLocation > dotSymbolLocations[0] && atSymbolLocation < dotSymbolLocations[1]){
+                if (mandatoryPrefixCheck(emailAddress, atSymbolLocation, dotSymbolLocations) &&
+                        optionalString1Check(emailAddress, atSymbolLocation, dotSymbolLocations)
+                        && mandatoryDomainCheck(emailAddress, atSymbolLocation, dotSymbolLocations)
+                        && optionalString2Check(emailAddress) && mandatoryString3Check(emailAddress,dotSymbolLocations, 4)){
+                    System.out.println("Case4 syntax is correct and matches Regex Pattern");
+                }
+                else {
+                    System.out.println("Case 4 syntax is correct but does not match Regex Patterns");
+                }
+            }
+            else {
+                System.out.println("Invalid Use case 4");
+            }
+        }
+
+
+        else if (atSymbolCount == 1 && dotSymbolCount == 2){
+
+            if(dotSymbolLocations[0] < atSymbolLocation && atSymbolLocation < dotSymbolLocations[1]){
+                if (mandatoryPrefixCheck(emailAddress, atSymbolLocation, dotSymbolLocations) &&
+                        optionalString1Check(emailAddress, atSymbolLocation, dotSymbolLocations) &&
+                        mandatoryDomainCheck(emailAddress, atSymbolLocation, dotSymbolLocations) &&
+                        mandatoryString3Check(emailAddress, dotSymbolLocations, 2)){
+                    System.out.println("Case 2 Syntax is Correct and matches Regex Pattern");
+                }
+                else {
+                    System.out.println("Case 2 Syntax is Correct but doesnot match Regex Pattern");
+                }
+            }
+
+            else if(atSymbolLocation < dotSymbolLocations[0]){
+                if (mandatoryPrefixCheck(emailAddress, atSymbolLocation, dotSymbolLocations)
+                        && mandatoryDomainCheck(emailAddress, atSymbolLocation, dotSymbolLocations)
+                        && optionalString2Check(emailAddress) && mandatoryString3Check(emailAddress,dotSymbolLocations, 3)){
+                    System.out.println("Case 3 Syntax is Correct and matches Regex Pattern");
+                }
+                else {
+                    System.out.println("Case 3 Syntax is Correct but doesnot match Regex Pattern");
+                }
+            }
+
+            else{
+                System.out.println("invalid Use Case 2 and 3");
+            }
+        }
+
+        else {
+            System.out.println("Invalid way of Writing email address");
+        }
+    }
+    //UC1
+    public static boolean mandatoryPrefixCheck(String emailAddress, int atSymbolLocation, int[] dotSymbolLocations){
+        String mandatoryPrefix;
+
+        if (atSymbolLocation < dotSymbolLocations[0]){
+            mandatoryPrefix = emailAddress.substring(0,atSymbolLocation);
+        }
+        else {
+            mandatoryPrefix = emailAddress.substring(0, dotSymbolLocations[0]);
+        }
+
+        Pattern pattern = Pattern.compile("^[a-zA-z][a-zA-Z0-9]*[+_-]?[a-zA-Z0-9]+$");
+        Matcher matcher = pattern.matcher(mandatoryPrefix);
+        return matcher.find();
+    }
+    //UC4
+    public static boolean optionalString1Check(String emailAddress, int atSymbolLocation, int[] dotSymbolLocations){
+        return true;
+    }
+
+    //UC2 method
+    public static boolean mandatoryDomainCheck(String emailAddress, int atSymbolLocation, int[] dotSymbolLocations){
+        return true;
+    }
+    //UC5
+    public static boolean optionalString2Check(String emailAddress){
+        return true;
+    }
+    //UC3
+    public static boolean mandatoryString3Check(String emailAddress, int[] dotSymbolLocations,int caseChecker){
+        return true;
+    }
+}
